d4dae965f2288fa5fab4e7ba77dede366ce4278d
JahongirIzzatullaev@m <jahongirizzatullaev@mail.ru>
2020-10-18 15:49:37 +0500

    deploy

---
+++ precache-manifest.75205fe68aae4de5a6427ca95ddf91f6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc520e5ca92bf17d5585",
+    "url": "/vue-project/css/app.736db063.css"
+  },
+  {
+    "revision": "42f3d0374fd36049f4da",
+    "url": "/vue-project/css/chunk-041b6dfe.0e922abe.css"
+  },
+  {
+    "revision": "0be945e6fde7e585cf88df8f6df655fa",
+    "url": "/vue-project/index.html"
+  },
+  {
+    "revision": "ac17e106953b1201896c",
+    "url": "/vue-project/js/about.9d21af5d.js"
+  },
+  {
+    "revision": "dc520e5ca92bf17d5585",
+    "url": "/vue-project/js/app.3c186ee2.js"
+  },
+  {
+    "revision": "42f3d0374fd36049f4da",
+    "url": "/vue-project/js/chunk-041b6dfe.292b9cc3.js"
+  },
+  {
+    "revision": "c4e0ae799afea11646f0",
+    "url": "/vue-project/js/chunk-2d0c9708.7925d616.js"
+  },
+  {
+    "revision": "197bf7349910543964a3",
+    "url": "/vue-project/js/chunk-2d0d05dc.4b0d7f9c.js"
+  },
+  {
+    "revision": "ae3dde3bdf4eb7f9241f",
+    "url": "/vue-project/js/chunk-2d20741e.580096f4.js"
+  },
+  {
+    "revision": "79855118da54f844b427",
+    "url": "/vue-project/js/chunk-2d217357.0193d66a.js"
+  },
+  {
+    "revision": "f16f649d3aeded6826ee",
+    "url": "/vue-project/js/chunk-2d2259e5.168d1851.js"
+  },
+  {
+    "revision": "d3b0cf8fbd99858530ff",
+    "url": "/vue-project/js/chunk-5f4ad635.4f089cc1.js"
+  },
+  {
+    "revision": "8db40779689b519a98ac",
+    "url": "/vue-project/js/chunk-c420df12.26a54d1d.js"
+  },
+  {
+    "revision": "7bda3ea5c780572e9320",
+    "url": "/vue-project/js/chunk-vendors.bd5c9314.js"
+  },
+  {
+    "revision": "89beb4233d787ee98192c81204838f2c",
+    "url": "/vue-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-project/robots.txt"
+  }
+]);
